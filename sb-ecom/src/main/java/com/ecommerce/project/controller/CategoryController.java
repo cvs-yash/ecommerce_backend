af1aeeae6fa5654c0329f6990664423a29767445
cvs-yash <yashwanth4335@gmail.com>
2026-01-16 11:15:43 +0530

    Initial commit

---
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.ecommerce.project.controller;
+
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+    List<Category> categories = categoryService.getALlCategories();
+    return new ResponseEntity<Stringv>()
+
+    }
+
+    @PostMapping("/api/public/categories")
+    public ResponseEntity<String> createCategory(@RequestBody Category category){
+        categoryService.createCategory(category);
+        return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+        try{
+            String status = categoryService.deleteCategory(categoryId);
+            //return new ResponseEntity<>(status, HttpStatus.OK);
+            return ResponseEntity.status(HttpStatus.OK).body(status);
+
+        }catch(ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
+
+
+    }
+}
